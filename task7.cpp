0f87ab44b331e1eebd6bfb95790ecbc30e624e24
Dek <karen.ananyan2017@mail.ru>
2023-04-09 22:05:41 +0600

    task 5

---
+++ task7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void print_permutations(string s, string prefix = "") {
+    if (s.length() == 0) {
+        cout << prefix << endl;
+    }
+    else {
+        for (int i = 0; i < s.length(); i++) {
+            string new_s = s.substr(0, i) + s.substr(i + 1);
+            print_permutations(new_s, s[i] + prefix);
+        }
+    }
+}
+
+int main() {
+    string s ;
+    cin >> s;
+    print_permutations(s);
+    return 0;
+}
