182194e053ea114d1f9d2fbd243c49c7b8fd9adc
Dek <karen.ananyan2017@mail.ru>
2023-04-09 18:57:53 +0600

    task 1

---
+++ task1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int find_min(int* arr, int n) {
+    if (n == 1) return *arr;
+    if (n < 1) throw invalid_argument("invalid lenght(must be more or equal to one)");
+    int min = find_min(arr, n - 1);
+    cout<< arr[n-2] << " " << min << endl;
+    if (arr[n-2] < min) {
+        return arr[n-2];
+    }
+    return min;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << find_min(arr, n);
+
+}
+
